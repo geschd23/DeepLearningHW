e42f5cee32b2679ff24da8fa3cfdc52e457f2a3d
Daniel Geschwender <geschd23@gmail.com>
2018-02-05 15:47:31 -0600

    Added the three required python files and Slurm submit script.

---
+++ Homework1/util.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+import math
+import os
+
+def split_data(data, proportion): #function taken from hackathon_3 notebook and improved by Jingchao Zhang
+    """
+    Split a numpy array into two parts of `proportion` and `1 - proportion`
+    
+    Args:
+        - data: numpy array, to be split along the first axis
+        - proportion: a float less than 1
+    """
+    size = data.shape[0]
+    np.random.seed(42)
+    s = np.random.permutation(size)
+    split_idx = int(proportion * size)
+    return data[s[:split_idx]], data[s[split_idx:]]
