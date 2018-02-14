c1c1a8064b8fdb9503e303231ec76ecdcdf39a9e
Daniel Geschwender <geschd23@gmail.com>
2018-02-13 22:58:09 -0600

    Modified main.py to work with hw2 dataset.
    Also modifies model.py with a convolutional block and dense convolutional block function.

--- Homework2/main.py
+++ Homework2/main.py
@@ -1,7 +1,7 @@
 #---------------------------------------------------
 # Daniel Geschwender, Yanfeng Liu, Jeevan Rajagopal
 # CSCE 496/896: Deep Learning
-# Homework 1
+# Homework 2
 #---------------------------------------------------
 # Based off code provided in Hackathon 3
 
@@ -13,18 +13,18 @@ import util
 import model
 
 flags = tf.app.flags
-flags.DEFINE_string('data_dir', '/work/cse496dl/shared/homework/01/', 'directory where FMNIST is located')
+flags.DEFINE_string('data_dir', '/work/cse496dl/shared/homework/02/', 'directory where EMODB/SAVEE is located')
 flags.DEFINE_string('save_dir', 'model', 'directory where model graph and weights are saved')
 flags.DEFINE_integer('batch_size', 32, '')
 flags.DEFINE_integer('max_epoch_num', 200, '')
 flags.DEFINE_integer('patience', 10, '')
-flags.DEFINE_string('architecture', "50", '')
+flags.DEFINE_string('filters', "2", '')
 flags.DEFINE_float('learning_rate', 0.0001, '')
 flags.DEFINE_float('dropout_rate', 0.0, '')
 flags.DEFINE_bool('l2_regularizer', False, '')
 flags.DEFINE_bool('output_model', False, '')
 flags.DEFINE_float('data_fraction', 1.0, '')
-flags.DEFINE_integer('num_folds', 10, '')
+flags.DEFINE_integer('fold', 4, '')
 FLAGS = flags.FLAGS
 
 def main(argv):
@@ -34,30 +34,32 @@ def main(argv):
     print("regularizer:",FLAGS.l2_regularizer)
     print("dropout_rate:",FLAGS.dropout_rate)
     print("learning_rate:",FLAGS.learning_rate)
-    print("architecture:",FLAGS.architecture)
+    print("filters:",FLAGS.filters)
     learning_rate = FLAGS.learning_rate
     dropout_rate = FLAGS.dropout_rate
-    architecture = list(map(int, FLAGS.architecture.split(",")))
+    filters = list(map(int, FLAGS.filters.split(",")))
     regularizer = tf.contrib.layers.l2_regularizer(scale=1.) if FLAGS.l2_regularizer else None
     
     # specify the network
-    input = tf.placeholder(tf.float32, [None, 784], name='input_placeholder')
+    input = tf.placeholder(tf.float32, [None, 16641], name='input_placeholder')
+    input2D = tf.reshape(input, [-1, 129, 129, 1])
     trainingMode = tf.placeholder(tf.bool)
-    network = model.create_model(input, trainingMode, architecture=architecture, regularizer=regularizer, dropout_rate=dropout_rate)
-    output = tf.identity(network, name='output')
+    network = model.conv_block(input2D, filters=filters, dropout_rate=dropout_rate, is_training=trainingMode)
+    flat = tf.reshape(network, [-1, 129*129*filters[-1]])
+    denseOut = tf.layers.dense(flat, 7)
+    output = tf.identity(denseOut, name='output')
 
     # define classification loss
-    label = tf.placeholder(tf.uint8, [None], name='label')
-    oneHot = tf.one_hot(label,10)
-    cross_entropy  = tf.nn.softmax_cross_entropy_with_logits(labels=oneHot, logits=output)
+    label = tf.placeholder(tf.uint8, [None, 7], name='label')
+    cross_entropy  = tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=output)
     reduce_mean_cross_entropy = tf.reduce_mean(cross_entropy)
     regularization_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
     REG_COEFF = 0.1
     total_loss = cross_entropy + REG_COEFF * sum(regularization_losses)
-
+    
     # setup confusion matrix and accuracy
-    confusion_matrix_op = tf.confusion_matrix(tf.argmax(oneHot, axis=1), tf.argmax(output, axis=1), num_classes=10)
-    accuracy = tf.equal(tf.argmax(oneHot, axis=1), tf.argmax(output, axis=1))
+    confusion_matrix_op = tf.confusion_matrix(tf.argmax(label, axis=1), tf.argmax(output, axis=1), num_classes=7)
+    accuracy = tf.equal(tf.argmax(label, axis=1), tf.argmax(output, axis=1))
     reduce_mean_accuracy = tf.reduce_mean(tf.cast(accuracy, tf.float32))
 
     # set up training and saving functionality
@@ -68,22 +70,22 @@ def main(argv):
     
     k_fold_accuracy = []
     
-    for fold in range(FLAGS.num_folds):
+    for fold in range(FLAGS.fold):
         print("Beginning fold ", fold)
         
         # load data
-        train_images = np.load(FLAGS.data_dir + 'fmnist_train_data.npy')
-        train_labels = np.load(FLAGS.data_dir + 'fmnist_train_labels.npy')
-
+        train_images = np.load(FLAGS.data_dir + 'EMODB-German/train_x_' + str(fold+1) + '.npy')
+        train_labels = np.load(FLAGS.data_dir + 'EMODB-German/train_y_' + str(fold+1) + '.npy')
+        validation_images = np.load(FLAGS.data_dir + 'EMODB-German/test_x_' + str(fold+1) + '.npy')
+        validation_labels = np.load(FLAGS.data_dir + 'EMODB-German/test_y_' + str(fold+1) + '.npy')
+        train_num_examples = train_images.shape[0]
+        validation_num_examples = validation_images.shape[0]
+        
         # reduce dataset according to data_fraction parameter
         train_images, _ = util.split_data(train_images, FLAGS.data_fraction)
         train_labels, _ = util.split_data(train_labels, FLAGS.data_fraction)
-
-        # split into train and validate
-        train_images, validation_images = util.k_fold_split(train_images, FLAGS.num_folds, fold)
-        train_labels, validation_labels = util.k_fold_split(train_labels, FLAGS.num_folds, fold)
-        train_num_examples = train_images.shape[0]
-        validation_num_examples = validation_images.shape[0]
+        validation_images, _ = util.split_data(validation_images, FLAGS.data_fraction)
+        validation_labels, _ = util.split_data(validation_labels, FLAGS.data_fraction)
 
         # set up early stopping
         best_epoch = 0
--- Homework2/model.py
+++ Homework2/model.py
@@ -1,41 +1,38 @@
 import tensorflow as tf
 
-def create_model(input, trainingMode, architecture, regularizer, dropout_rate):
+# Based on function provided in Hackathon 5
+def conv_block(inputs, filters, dropout_rate, is_training):
     """
-    Generates a block of tensors
-
     Args:
-        - input: the input tensor
-        - trainingMode: a scalar tensor to toggle training mode for dropout
-        - architecture: array indicating the number of nodes to include in dense layers
-        - regularizer: the regularization function to use
-        - dropout_rate: probability of dropping a node during dropout
+        - inputs: 4D tensor of shape NHWC
+        - filters: iterable of ints
+        - dropout_rate: chance of dropping a node
+        - is_training: boolean scalar tensor
     """
-    # normalize the input
-    layers = [tf.divide(input,255.0)]
-    
-    with tf.name_scope('linear_model') as scope:
-        # dropout on input
-        layers.append(tf.layers.Dropout(dropout_rate))
-        
-        # construct each hidden layer
-        for nodes in architecture:
-            layers.append(tf.layers.Dense(nodes,
-                             kernel_regularizer=regularizer,
-                             bias_regularizer=regularizer,
-                             activation=tf.nn.relu))
-            layers.append(tf.layers.Dropout(dropout_rate))
-            
-        # construct the output layer
-        layers.append(tf.layers.Dense(10,
-                             kernel_regularizer=regularizer,
-                             bias_regularizer=regularizer))
+    with tf.name_scope('conv_block') as scope:
+        x = inputs
+        for i in range(len(filters)):
+            x = tf.layers.dropout(x, dropout_rate, training=is_training)
+            x = tf.layers.conv2d(x, filters[0], 3, 1, padding='same')
+            x = tf.layers.batch_normalization(x, training=is_training)
+            x = tf.nn.elu(x)
+    return x
 
-        # construct tensors from tensor objects
-        for i in range(len(layers)-1):
-            if i%2==0:  # dropout layer
-                layers[i+1]=layers[i+1](layers[i], training=trainingMode)
-            else: # non-dropout layer
-                layers[i+1]=layers[i+1](layers[i])
-                
-    return layers[len(layers)-1]
+def dense_conv_block(inputs, filters, dropout_rate, is_training):
+    """
+    Args:
+        - inputs: 4D tensor of shape NHWC
+        - filters: iterable of ints
+        - dropout_rate: chance of dropping a node
+        - is_training: boolean scalar tensor
+    """
+    with tf.name_scope('conv_block') as scope:
+        concatenation = inputs
+        for i in range(len(filters)):
+            x = concatenation
+            x = tf.layers.dropout(x, dropout_rate, training=is_training)
+            x = tf.layers.conv2d(x, filters[0], 3, 1, padding='same')
+            x = tf.layers.batch_normalization(x, training=is_training)
+            x = tf.nn.elu(x)
+            concatenation = tf.concat([concatenation, x], 3)
+    return x
