0c19d098d412217b28b695b9164d0526f38f720a
Daniel Geschwender <geschd23@gmail.com>
2018-03-05 12:17:32 -0600

    parseOutput.sh prints the output filename

--- Homework2/parseOuput.sh
+++ Homework2/parseOuput.sh
@@ -2,6 +2,7 @@
 
 for f in $(ls $1/*.out);
 do
+	echo -n "$f, "
 	for l in $(cat $f | grep "regularizer\|dropout_rate\|learning_rate\|filters\|linear_nodes\|Final VALIDATION ACCURACY\|Average accuracy across k folds" | sed 's/ //g');
 	do
 		echo -n '"'
