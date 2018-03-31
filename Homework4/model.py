aad6c41974ba50c84c83f16fc65bed132d1d0bd1
Daniel Geschwender <geschd23@gmail.com>
2018-03-30 19:08:47 -0500

    Adds a dataset and reworks model

--- Homework4/model.py
+++ Homework4/model.py
@@ -1,13 +1,25 @@
 import tensorflow as tf
 import numpy as np
 
-def lstm(input, layer_nodes, batch_size):
-    layers = [tf.nn.rnn_cell.LSTMCell(nodes) for nodes in layer_nodes]
-    multicell = tf.nn.rnn_cell.MultiRNNCell(layers)
-    x, state = tf.nn.dynamic_rnn(cell=multicell,
-                                       inputs=input,
-                                       dtype=tf.float32)
-    x = tf.layers.dense(x, input.get_shape().as_list()[2])
+def sentence_completion_rnn(encoder_input, decoder_input, layer_nodes, batch_size):
+    
+    # encoder
+    with tf.variable_scope('encoder'):
+        encoder_layers = [tf.nn.rnn_cell.LSTMCell(nodes) for nodes in layer_nodes]
+        encoder_multicell = tf.nn.rnn_cell.MultiRNNCell(encoder_layers)
+        encoder_output, encoder_state = tf.nn.dynamic_rnn(cell=encoder_multicell,
+                                           inputs=encoder_input,
+                                           dtype=tf.float32)
+    
+    # decoder
+    with tf.variable_scope('decoder'):
+        decoder_layers = [tf.nn.rnn_cell.LSTMCell(nodes) for nodes in layer_nodes]
+        decoder_multicell = tf.nn.rnn_cell.MultiRNNCell(decoder_layers)
+        decoder_output, decoder_state = tf.nn.dynamic_rnn(cell=decoder_multicell,
+                                           inputs=decoder_input,
+                                           dtype=tf.float32)
+    
+    x = tf.layers.dense(decoder_output, encoder_input.get_shape().as_list()[2])
     x = tf.tanh(x)
     x = x / tf.norm(x, axis=2, keep_dims=True)
     return x
