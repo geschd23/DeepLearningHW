4a7379c1d916609b90341520916eb875dc367342
Daniel Geschwender <geschd23@gmail.com>
2018-04-12 15:57:19 -0500

    Agent now trains using Actor-critic
    It is currently single threaded and not yet clear
    whether training improves performance.

--- Project/pysc2-master/pysc2/agents/model.py
+++ Project/pysc2-master/pysc2/agents/model.py
@@ -13,27 +13,31 @@ def convolution(input, filters, size, regularizer, dropout_rate, training) :
     x = tf.nn.elu(x)
     return x
 
-def sc2network():
-    screen_input = tf.placeholder(tf.float32, [None, 64, 64, 17], name='screen_input')
-    minimap_input = tf.placeholder(tf.float32, [None, 64, 64, 7], name='minimap_input')
-    player_input = tf.placeholder(tf.float32, [None, 11], name='player_input')
-    single_select_input = tf.placeholder(tf.float32, [None, 7], name='single_select_input')
-    action_mask = tf.placeholder(tf.float32, [None, 524], name='action_mask')
-    
-    regularizer = tf.contrib.layers.l2_regularizer(scale=1.)
-    dropout_rate = 0.0
-    training = tf.placeholder_with_default(False, [], name='training_flag')
-    
-    with tf.variable_scope('sc2network'):
-        with tf.variable_scope('screen'):
-            screen = screen_input
-            screen = convolution(screen, 16, 5, regularizer, dropout_rate, training)
-            screen = convolution(screen, 32, 3, regularizer, dropout_rate, training)
+def sc2network(optimizer, beta, eta, scope):
+    with tf.variable_scope(scope):
+        regularizer = tf.contrib.layers.l2_regularizer(scale=1.)
+        dropout_rate = 0.0
+        training = tf.placeholder_with_default(False, [], name='training_flag')
+        
+        
+        with tf.variable_scope('observation'):
             
-        with tf.variable_scope('minimap'):
-            minimap = minimap_input
-            minimap = convolution(minimap, 16, 5, regularizer, dropout_rate, training)
-            minimap = convolution(minimap, 32, 3, regularizer, dropout_rate, training)
+            screen_input = tf.placeholder(tf.float32, [None, 64, 64, 17], name='screen_input')
+            minimap_input = tf.placeholder(tf.float32, [None, 64, 64, 7], name='minimap_input')
+            player_input = tf.placeholder(tf.float32, [None, 11], name='player_input')
+            single_select_input = tf.placeholder(tf.float32, [None, 7], name='single_select_input')
+            action_mask = tf.placeholder(tf.float32, [None, 524], name='action_mask')
+
+            with tf.variable_scope('screen'):
+                screen = screen_input
+                screen = convolution(screen, 16, 5, regularizer, dropout_rate, training)
+                screen = convolution(screen, 32, 3, regularizer, dropout_rate, training)
+
+            with tf.variable_scope('minimap'):
+                minimap = minimap_input
+                minimap = convolution(minimap, 16, 5, regularizer, dropout_rate, training)
+                minimap = convolution(minimap, 32, 3, regularizer, dropout_rate, training)
+            
             
         with tf.variable_scope('state'):
             broadcast_player_input = tf.tile(tf.reshape(player_input, [-1,1,1,11]), [1, 64, 64, 1])
@@ -41,73 +45,131 @@ def sc2network():
             full_state = tf.concat([screen, minimap, broadcast_player_input, broadcast_single_select_input], axis=3)
             dense_state = dense(tf.contrib.layers.flatten(full_state), 256, regularizer, dropout_rate, training)
             
+            
         with tf.variable_scope('value'):
             value = dense(dense_state, 1, regularizer, dropout_rate, training)
             
-        with tf.variable_scope('action'):
-            action_logits = dense(dense_state, 524, regularizer, dropout_rate, training)
-            masked_action_logits = action_logits-1e32*(1-action_mask)
-            action = tf.multinomial(masked_action_logits, 1)
-            
-        with tf.variable_scope('param_screen'):
-            param_screen_logits = convolution(full_state, 1, 1, regularizer, dropout_rate, training)
-            param_screen = tf.multinomial(tf.contrib.layers.flatten(param_screen_logits), 1)
-            param_screen = tf.concat([param_screen//64, param_screen%64], axis=1)
-            
-        with tf.variable_scope('param_minimap'):
-            param_minimap_logits = convolution(full_state, 1, 1, regularizer, dropout_rate, training)
-            param_minimap = tf.multinomial(tf.contrib.layers.flatten(param_minimap_logits), 1)
-            param_minimap = tf.concat([param_minimap//64, param_minimap%64], axis=1)
-            
-        with tf.variable_scope('param_screen2'):
-            param_screen2_logits = convolution(full_state, 1, 1, regularizer, dropout_rate, training)
-            param_screen2 = tf.multinomial(tf.contrib.layers.flatten(param_screen2_logits), 1)
-            param_screen2 = tf.concat([param_screen2//64, param_screen2%64], axis=1)
-            
-        with tf.variable_scope('param_queued'):
-            param_queued_logits = dense(dense_state, 2, regularizer, dropout_rate, training)
-            param_queued = tf.multinomial(param_queued_logits, 1)
-            
-        with tf.variable_scope('param_control_group_act'):
-            param_control_group_act_logits = dense(dense_state, 5, regularizer, dropout_rate, training)
-            param_control_group_act = tf.multinomial(param_control_group_act_logits, 1)
-            
-        with tf.variable_scope('param_control_group_id'):
-            param_control_group_id_logits = dense(dense_state, 10, regularizer, dropout_rate, training)
-            param_control_group_id = tf.multinomial(param_control_group_id_logits, 1)
-            
-        with tf.variable_scope('param_select_point_act'):
-            param_select_point_act_logits = dense(dense_state, 4, regularizer, dropout_rate, training)
-            param_select_point_act = tf.multinomial(param_select_point_act_logits, 1)
-            
-        with tf.variable_scope('param_select_add'):
-            param_select_add_logits = dense(dense_state, 2, regularizer, dropout_rate, training)
-            param_select_add = tf.multinomial(param_select_add_logits, 1)
-            
-        with tf.variable_scope('param_select_unit_act'):
-            param_select_unit_act_logits = dense(dense_state, 4, regularizer, dropout_rate, training)
-            param_select_unit_act = tf.multinomial(param_select_unit_act_logits, 1)
-            
-        with tf.variable_scope('param_select_unit_id'):
-            param_select_unit_id_logits = dense(dense_state, 500, regularizer, dropout_rate, training)
-            param_select_unit_id = tf.multinomial(param_select_unit_id_logits, 1)
-            
-        with tf.variable_scope('param_select_worker'):
-            param_select_worker_logits = dense(dense_state, 4, regularizer, dropout_rate, training)
-            param_select_worker = tf.multinomial(param_select_worker_logits, 1)
-            
-        with tf.variable_scope('param_build_queue_id'):
-            param_build_queue_id_logits = dense(dense_state, 10, regularizer, dropout_rate, training)
-            param_build_queue_id = tf.multinomial(param_build_queue_id_logits, 1)
-            
-        with tf.variable_scope('param_unload_id'):
-            param_unload_id_logits = dense(dense_state, 500, regularizer, dropout_rate, training)
-            param_unload_id = tf.multinomial(param_unload_id_logits, 1)
-            
-        param_list = [param_screen, param_minimap, param_screen2, param_queued,
-                      param_control_group_act, param_control_group_id,
-                      param_select_point_act, param_select_add, param_select_unit_act,
-                      param_select_unit_id, param_select_worker, param_build_queue_id,
-                      param_unload_id]
         
-    return (screen_input, minimap_input, player_input, single_select_input, action_mask, action, param_list)
+        with tf.variable_scope('policy'):
+
+            with tf.variable_scope('action'):
+                action_logits = dense(dense_state, 524, regularizer, dropout_rate, training)
+                masked_action_logits = action_logits-1e32*(1-action_mask)
+                action_policy = tf.nn.softmax(masked_action_logits)
+
+            with tf.variable_scope('param_screen'):
+                param_screen_logits = convolution(full_state, 1, 1, regularizer, dropout_rate, training)
+                param_screen_logits = tf.contrib.layers.flatten(param_screen_logits)
+                param_screen_policy = tf.nn.softmax(param_screen_logits)
+
+            with tf.variable_scope('param_minimap'):
+                param_minimap_logits = convolution(full_state, 1, 1, regularizer, dropout_rate, training)
+                param_minimap_logits = tf.contrib.layers.flatten(param_minimap_logits)
+                param_minimap_policy = tf.nn.softmax(param_minimap_logits)
+
+            with tf.variable_scope('param_screen2'):
+                param_screen2_logits = convolution(full_state, 1, 1, regularizer, dropout_rate, training)
+                param_screen2_logits = tf.contrib.layers.flatten(param_screen2_logits)
+                param_screen2_policy = tf.nn.softmax(param_screen2_logits)
+
+            with tf.variable_scope('param_queued'):
+                param_queued_logits = dense(dense_state, 2, regularizer, dropout_rate, training)
+                param_queued_policy = tf.nn.softmax(param_queued_logits)
+
+            with tf.variable_scope('param_control_group_act'):
+                param_control_group_act_logits = dense(dense_state, 5, regularizer, dropout_rate, training)
+                param_control_group_act_policy = tf.nn.softmax(param_control_group_act_logits)
+
+            with tf.variable_scope('param_control_group_id'):
+                param_control_group_id_logits = dense(dense_state, 10, regularizer, dropout_rate, training)
+                param_control_group_id_policy = tf.nn.softmax(param_control_group_id_logits)
+
+            with tf.variable_scope('param_select_point_act'):
+                param_select_point_act_logits = dense(dense_state, 4, regularizer, dropout_rate, training)
+                param_select_point_act_policy = tf.nn.softmax(param_select_point_act_logits)
+
+            with tf.variable_scope('param_select_add'):
+                param_select_add_logits = dense(dense_state, 2, regularizer, dropout_rate, training)
+                param_select_add_policy = tf.nn.softmax(param_select_add_logits)
+
+            with tf.variable_scope('param_select_unit_act'):
+                param_select_unit_act_logits = dense(dense_state, 4, regularizer, dropout_rate, training)
+                param_select_unit_act_policy = tf.nn.softmax(param_select_unit_act_logits)
+
+            with tf.variable_scope('param_select_unit_id'):
+                param_select_unit_id_logits = dense(dense_state, 500, regularizer, dropout_rate, training)
+                param_select_unit_id_policy = tf.nn.softmax(param_select_unit_id_logits)
+
+            with tf.variable_scope('param_select_worker'):
+                param_select_worker_logits = dense(dense_state, 4, regularizer, dropout_rate, training)
+                param_select_worker_policy = tf.nn.softmax(param_select_worker_logits)
+
+            with tf.variable_scope('param_build_queue_id'):
+                param_build_queue_id_logits = dense(dense_state, 10, regularizer, dropout_rate, training)
+                param_build_queue_id_policy = tf.nn.softmax(param_build_queue_id_logits)
+
+            with tf.variable_scope('param_unload_id'):
+                param_unload_id_logits = dense(dense_state, 500, regularizer, dropout_rate, training)
+                param_unload_id_policy = tf.nn.softmax(param_unload_id_logits)
+
+            param_policy = [param_screen_policy, param_minimap_policy, param_screen2_policy, param_queued_policy,
+                          param_control_group_act_policy, param_control_group_id_policy,
+                          param_select_point_act_policy, param_select_add_policy, param_select_unit_act_policy,
+                          param_select_unit_id_policy, param_select_worker_policy, param_build_queue_id_policy,
+                          param_unload_id_policy]
+
+            
+        if scope != 'global':
+            with tf.variable_scope('loss'):
+
+                with tf.variable_scope('loss_inputs'):
+                    advantage_input = tf.placeholder(tf.float32, [None, 1], name='advantage_input')
+                    target_value_input = tf.placeholder(tf.float32, [None, 1], name='target_value_input')
+
+                with tf.variable_scope('action_inputs'):
+                    action_input = tf.placeholder(tf.int32, [None, 1], name='action_input')
+                    param_screen_input = tf.placeholder(tf.int32, [None, 1], name='param_screen_input')
+                    param_minimap_input = tf.placeholder(tf.int32, [None, 1], name='param_minimap_input')
+                    param_screen2_input = tf.placeholder(tf.int32, [None, 1], name='param_screen2_input')
+                    param_queued_input = tf.placeholder(tf.int32, [None, 1], name='param_queued_input')
+                    param_control_group_act_input = tf.placeholder(tf.int32, [None, 1], name='param_control_group_act_input')
+                    param_control_group_id_input = tf.placeholder(tf.int32, [None, 1], name='param_control_group_id_input')
+                    param_select_point_act_input = tf.placeholder(tf.int32, [None, 1], name='param_select_point_act_input')
+                    param_select_add_input = tf.placeholder(tf.int32, [None, 1], name='param_select_add_input')
+                    param_select_unit_act_input = tf.placeholder(tf.int32, [None, 1], name='param_select_unit_act_input')
+                    param_select_unit_id_input = tf.placeholder(tf.int32, [None, 1], name='param_select_unit_id_input')
+                    param_select_worker_input = tf.placeholder(tf.int32, [None, 1], name='param_select_worker_input')
+                    param_build_queue_id_input = tf.placeholder(tf.int32, [None, 1], name='param_build_queue_id_input')
+                    param_unload_id_input = tf.placeholder(tf.int32, [None, 1], name='param_unload_id_input')
+
+                    param_input = [param_screen_input, param_minimap_input, param_screen2_input, param_queued_input,
+                              param_control_group_act_input, param_control_group_id_input,
+                              param_select_point_act_input, param_select_add_input, param_select_unit_act_input,
+                              param_select_unit_id_input, param_select_worker_input, param_build_queue_id_input,
+                              param_unload_id_input]
+
+                with tf.variable_scope('policy_loss'):
+                    action_policy_used = tf.reduce_sum(tf.one_hot(action_input, 524) * action_policy, axis=1)
+                    param_policy_used = [ tf.reduce_sum(tf.one_hot(param_input[i], param_policy[i].get_shape().as_list()[-1]) * param_policy[i], axis=1) for i in range(len(param_policy))]
+                    policy_loss = tf.reduce_sum(advantage_input * tf.log(tf.concat([action_policy_used]+param_policy_used, axis=1)+1E-5))
+
+                with tf.variable_scope('entropy_loss'):
+                    action_entropy = action_policy * tf.log(action_policy+1E-5)
+                    param_entropy = [ param_policy[i] * tf.log(param_policy[i]+1E-5) for i in range(len(param_policy))]
+                    entropy_loss = tf.reduce_sum(tf.concat([action_entropy]+param_entropy, axis=1))
+
+                with tf.variable_scope('value_loss'):
+                    value_loss = tf.reduce_sum(tf.square(target_value_input-value))
+
+                with tf.variable_scope('total_loss'):
+                    total_loss = -policy_loss + beta*value_loss + eta*entropy_loss
+
+                with tf.variable_scope('gradients'):
+                    local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope)
+                    gradients = tf.gradients(total_loss, local_vars)
+                    global_norm = tf.global_norm(local_vars)
+                    clipped_gradients, clipped = tf.clip_by_global_norm(gradients,1.0)
+                    update_step = optimizer.apply_gradients(zip(clipped_gradients, local_vars))
+                        
+        
+    return (screen_input, minimap_input, player_input, single_select_input, action_mask, action_policy, param_policy, value, action_input, param_input, advantage_input, target_value_input, gradients, update_step, global_norm, clipped)
--- Project/pysc2-master/pysc2/agents/rl_agent.py
+++ Project/pysc2-master/pysc2/agents/rl_agent.py
@@ -21,6 +21,7 @@ import numpy as np
 import tensorflow as tf
 import pysc2.agents.model as model
 from datetime import datetime
+import copy
 
 from pysc2.agents import base_agent
 from pysc2.lib import actions
@@ -31,12 +32,19 @@ class RlAgent(base_agent.BaseAgent):
     """A Deep RL agent for starcraft."""
     def __init__(self):
         super(RlAgent,self).__init__()
-        self.screen_input, self.minimap_input, self.player_input, self.single_select_input, self.action_mask, self.action, self.param_list = model.sc2network()
+        self.screen_input, self.minimap_input, self.player_input, self.single_select_input, self.action_mask, self.action_policy, self.param_policy, self.value, self.action_input, self.param_input, self.advantage_input, self.target_value_input, self.gradients, self.update_step, self.global_norm, self.clipped = model.sc2network(tf.train.RMSPropOptimizer(0.001), beta=0.5, eta=0.01, scope="local")
         self.session = tf.Session()
         self.session.run(tf.global_variables_initializer())
+        self.replay_buffer = []
+        self.t_max = 40
+        print("constructing agent", id(self))
+        print("Network parameters", np.sum([np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()]))
 
     def step(self, obs):
         super(RlAgent, self).step(obs)
+        
+        if len(self.replay_buffer) == self.t_max: 
+            self.update()
         
         action_mask = np.zeros([1,524])
         for i in obs.observation["available_actions"]:
@@ -49,10 +57,58 @@ class RlAgent(base_agent.BaseAgent):
             self.single_select_input: np.swapaxes(np.reshape(np.array(obs.observation["single_select"]), [7,1]),0,1),
             self.action_mask: action_mask}
         
-        function_id, function_params = self.session.run([self.action, self.param_list], feed_dict)
+        action_policy, param_policy, value = self.session.run([self.action_policy, self.param_policy, self.value], feed_dict)
+           
+        action = [[np.random.choice(range(len(action_policy[0])), p=action_policy[0])]]
+        params = [[[np.random.choice(range(len(param[0])), p=param[0])]] for param in param_policy]  
         
-        function_id = function_id[0][0]        
-        args = [function_params[arg.id][0] for arg in self.action_spec.functions[function_id].args]
-        print("selected action: ",function_id, args)
+        self.replay_buffer.append({"obs":obs, "action":action, "params":params, "value":value})
+                  
+        function = action[0][0]
+        # handle spatial params
+        temp = copy.deepcopy(params)
+        temp[0][0] = [params[0][0][0]//64, params[0][0][0]%64] # screen
+        temp[1][0] = [params[1][0][0]//64, params[1][0][0]%64] # minimap
+        temp[2][0] = [params[2][0][0]//64, params[2][0][0]%64] # screen2
+        args = [temp[arg.id][0] for arg in self.action_spec.functions[function].args]
+        print("selected action: ",function, args)
+              
+        return actions.FunctionCall(function, args)
+    
+    def update(self):
+        print("update")
         
-        return actions.FunctionCall(function_id, args)
+        if len(self.replay_buffer) == self.t_max: 
+            R = self.replay_buffer[-1]["value"][0][0]
+        else:
+            R = 0
+            
+        for i in reversed(range(len(self.replay_buffer))):
+            obs = self.replay_buffer[i]["obs"]
+            action = self.replay_buffer[i]["action"]
+            params = self.replay_buffer[i]["params"]
+            value = self.replay_buffer[i]["value"][0][0]
+            advantage = R - value
+
+            action_mask = np.zeros([1,524])
+            for i in obs.observation["available_actions"]:
+                action_mask[0][i]=1
+            
+            feed_dict = {
+                self.screen_input: np.swapaxes(np.reshape(np.array(obs.observation["screen"]), [17,64,64,1]),0,3),
+                self.minimap_input: np.swapaxes(np.reshape(np.array(obs.observation["minimap"]), [7,64,64,1]),0,3),
+                self.player_input: np.swapaxes(np.reshape(np.array(obs.observation["player"]), [11,1]),0,1),
+                self.single_select_input: np.swapaxes(np.reshape(np.array(obs.observation["single_select"]), [7,1]),0,1),
+                self.action_mask: action_mask,
+                self.action_input: action,
+                self.advantage_input: [[advantage]],
+                self.target_value_input: [[R]]}
+            feed_dict.update({i: d for i, d in zip(self.param_input, params)})
+            
+            norm, clipped, _ = self.session.run([self.global_norm, self.clipped, self.update_step] , feed_dict)
+            print(norm, clipped)
+            
+            # compute next return
+            R = obs.reward + obs.discount*R
+            
+        self.replay_buffer = []
